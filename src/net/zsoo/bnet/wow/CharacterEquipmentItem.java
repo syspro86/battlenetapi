645aa2ef03f0eb4cb8e17f7f6c9869e16567577e
kiho <syspro86@gmail.com>
2019-12-08 19:07:58 +0900

    1

--- src/net/zsoo/bnet/wow/CharacterEquipmentItem.java
+++ src/net/zsoo/bnet/wow/CharacterEquipmentItem.java
@@ -2,9 +2,11 @@ package net.zsoo.bnet.wow;
 
 import java.lang.reflect.Type;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
@@ -264,8 +266,8 @@ public class CharacterEquipmentItem {
 
 		@Override
 		public String deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-			String str = json.getAsString();
-			return str;
+			Gson gson = new Gson();
+			return gson.toJson(json);
 		}
 	}
 }
